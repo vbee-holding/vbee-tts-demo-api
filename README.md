f040bba4add3e7d89a6a14415018f607b04c53eb
macvanthiem <macvanthiem1999@gmail.com>
2021-11-15 14:02:52 +0700

    add convert-text

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # vbee-tts-demo-api
+
+## Installation
+
+```
+npm install
+```
+
+## Running
+
+```
+npm start
+```
+
+## Api docs
+
+| Method | URI                         | Action                    |
+| ------ | --------------------------- | ------------------------- |
+| GET    | /api/voices                 | Get voice list            |
+| GET    | /api/synthesis-techs        | Get voice technology list |
+| GET    | /api/tts?voice=&input_text= | Đọc văn bản (fake)        |
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const logger = require("morgan");
+const voiceRoute = require("./routes/voice");
+
+require("dotenv").config();
+require("./models");
+
+// Middleware
+app.use(logger("dev"));
+app.use(cors());
+
+// Routes
+app.get("/api", (req, res, next) => {
+    return res.send({ status: 1, message: "Server is OK!" });
+});
+
+app.use("/api", voiceRoute);
+
+// 404
+app.use((req, res, next) => {
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+});
+
+// Handle error
+app.use((err, req, res, next) => {
+    const error = app.get("env") === "development" ? err : {};
+    const status = err.status || 500;
+
+    return res.status(status).json({
+        error: {
+            message: error.message,
+        },
+    });
+});
+
+// Start server
+const { PORT } = process.env;
+const port = PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}!`);
+});
